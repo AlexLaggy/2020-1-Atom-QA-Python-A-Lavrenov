eaee086b129f3d5f374d0f2ce79b4a47dcb4646a
Laggystick <a.lavrenov@skib-dozen.org>
2020-05-14 15:46:02 +0300

    Fixed addr for Jenkins without wb/hub with network

--- Final_Project/ui/fixtures.py
+++ Final_Project/ui/fixtures.py
@@ -60,7 +60,7 @@ def driver(config):
                             'browserName': 'chrome',
                             'version': version,
                             }
-            driver = webdriver.Remote(command_executor=f'http://{selenoid}/wd/hub',
+            driver = webdriver.Remote(command_executor=f'http://{selenoid}/',
                                       options=options,
                                       desired_capabilities=capabilities
                                       )
