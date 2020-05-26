5b078a819b6d6a55cc274a76d5dc339b44156309
Laggystick <a.lavrenov@skib-dozen.org>
2020-05-26 17:44:21 +0300

    Fix dictionary in vo_mock

--- Final_Project/mock/vk.py
+++ Final_Project/mock/vk.py
@@ -1,37 +1,10 @@
 import threading
 
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.orm import sessionmaker, mapper
 from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-
-class Users(object):
-    pass
-
-
-class DBUser:
-    def __init__(self, login, password, db, host, port):
-        self.host = host
-        self.port = port
-        self.db = db
-        self.login = login
-        self.password = password
-        self.table = Users
-        self.session = self.connect()
-
-    def connect(self):
-        url = f'mysql+pymysql://{self.login}:{self.password}@{self.host}:{self.port}/{self.db}'
-
-        engine = create_engine(url)
-        metadata = MetaData(engine)
-        test_users = Table('test_users', metadata, autoload=True)
-        mapper(Users, test_users)
-
-        session = sessionmaker(bind=engine, autocommit=True, enable_baked_queries=False)
-
-        return session()
+users = {'Akkakiy13': 0, 'Test': 1}
 
 
 def run_mock(host, port):
@@ -51,12 +24,17 @@ def db_select(self, field, value):
     return self.db.session.query(self.db.table).filter(field == value).first()
 
 
+@app.route('/vk_id', methods=['POST'])
+def create_user():
+    user = request.form['user']
+    users.update({user: len(users)})
+
+
 @app.route('/vk_id/<username>')
 def get_vk_id(username: str):
-    a.session.expire_all()
-    user = a.session.query(a.table).filter(a.table.username == username).first()
-    if user:
-        return {'vk_id': user.id}, 200
+    user_id = users.get(username)
+    if user_id:
+        return {'vk_id': user_id}, 200
     else:
         abort(404)
 
@@ -68,4 +46,3 @@ def shutdown():
 
 if __name__ == '__main__':
     run_mock('0.0.0.0', 5000)
-    a = DBUser('test_qa', 'qa_test', 'test', 'mysql_database', '3306')
